70384257b17a793abebd598a1bce8124afb61f0a
CHIA HONG CHANG <vic147569az@gmail.com>
2024-06-17 16:36:29 +0800

    feat(board): add board page

---
+++ actions/create-board.ts
@@ -0,0 +1,49 @@
+'use server'
+
+import { db } from '@/lib/db'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
+export type State = {
+  errors?: {
+    title?: string[]
+  }
+  message?: string | null
+}
+
+const CreateBoard = z.object({
+  title: z.string().min(3, {
+    message: 'Minimum length of 3 letters is required'
+  })
+})
+
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = CreateBoard.safeParse({
+    title: formData.get('title')
+  })
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields.'
+    }
+  }
+
+  const { title } = validatedFields.data
+
+  try {
+    await db.board.create({
+      data: {
+        title
+      }
+    })
+  } catch (error) {
+    return {
+      message: 'Database Error'
+    }
+  }
+
+  revalidatePath('/organization/org_2hz8gSs9pteSSLuCzW3kp8l1nMr')
+  redirect('/organization/org_2hz8gSs9pteSSLuCzW3kp8l1nMr')
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,19 @@
-const OrganizationPage = () => {
-  return <div>Organization</div>
+import { db } from '@/lib/db'
+import { Board } from './board'
+import { Form } from './form'
+
+const OrganizationPage = async () => {
+  const boards = await db.board.findMany()
+
+  return (
+    <div className=" flex flex-col space-y-4">
+      <Form />
+      <div className=" space-y-2">
+        {boards.map((board) => (
+          <Board title={board.title} id={board.id} key={board.id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 export default OrganizationPage

e8c3fa50e75be5bd164198b9dc62fab29c28c1a8
CHIA HONG CHANG <vic147569az@gmail.com>
2024-06-17 11:59:15 +0800

    feat(sidebar): add sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,11 @@
+import { OrgControl } from './_components/org-control'
+
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  )
+}
+export default OrganizationLayout
